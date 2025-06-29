dfa1a56dc169256ed18418060521bb8514e2343e
Raimundo Pardo <153950844+rpard@users.noreply.github.com>
2025-06-29 16:23:42 -0400

    Exercises 4.8.-4.12.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,27 +1,25 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-    const blog = new Blog({
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes || 0,
-    })
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes !== undefined ? body.likes : 0,
+  })
 
-    blog.save().then(savedBlog => {
-        response.json(savedBlog)
-    }).catch(error => next(error))
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 
 })
-    
+
 
 module.exports = blogsRouter
